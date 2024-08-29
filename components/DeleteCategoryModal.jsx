eb2cdb26e55f09438a179058108fb3af88628b35
Ricardo <ricardo.kanashiro.adm@gmail.com>
2024-08-29 00:29:50 -0300

    adding: ⭐ deleteCategoryModal funcionality

--- components/DeleteCategoryModal.jsx
+++ components/DeleteCategoryModal.jsx
@@ -1,12 +1,26 @@
 import { useContext } from "react"
 
 import { ModalsContext } from "../contexts/ModalsContext"
+import { DataContext } from "../contexts/DataContext"
+import { NavigationContext } from "../contexts/NavigationContext"
 
 import "../css/components/delete-category-modal.css"
 
 const DeleteCategoryModal = () => {
 
    const { toggleDeleteCategoryModal, toDeleteCategory, deleteCategoryModalActive } = useContext(ModalsContext)
+   const { setSharedCategorias } = useContext(DataContext)
+   const { setSelectedSection } = useContext(NavigationContext)
+
+   function deleteCategory() {
+
+      console.log(toDeleteCategory)
+
+      setSharedCategorias(categorias => categorias.filter(categoria => categoria.nome !== toDeleteCategory))
+
+      toggleDeleteCategoryModal()
+      setSelectedSection("categorias")
+   }
 
    return (
       <div className={deleteCategoryModalActive ? "delete-category-modal" : "delete-category-modal--disabled"}>
@@ -21,7 +35,7 @@ const DeleteCategoryModal = () => {
 
          <div className="delete-category-modal__buttons-area">
             <button className="delete-category-modal__cancelar-btn" onClick={toggleDeleteCategoryModal}>Cancelar</button>
-            <button className="delete-category-modal__deletar-btn">Deletar</button>
+            <button className="delete-category-modal__deletar-btn" onClick={deleteCategory}>Deletar</button>
          </div>
       </div>
    )
