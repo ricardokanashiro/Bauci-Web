8ddeae83463bd619fc19444f6f81353c56b46158
Gabriella Melo <gabriella.melo0119@gmail.com>
2024-08-30 02:24:39 -0300

    fixing ⭐ conflicts with git stash pop on _categorias.css.map

--- data.js
+++ data.js
@@ -1,88 +1,291 @@
 export let categorias = [
-    {
-        nome: "Cozinheiro",
-        produtos: [
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "1",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate em Caixa",
-               prazoMinimo: "1",
-               prazoMaximo: "2",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Outro Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "3",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         ]
-    },
+   {
+      nome: "Cozinheiro",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
 
-    {
-        nome: "Outro Cozinheiro",
-        produtos: [
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "1",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate em Caixa",
-               prazoMinimo: "1",
-               prazoMaximo: "2",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Outro Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "3",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         ]
-    },
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
 
-    {
-        nome: "Atendente",
-        produtos: [
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "1",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Tomate em Caixa",
-               prazoMinimo: "1",
-               prazoMaximo: "2",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         
-            {
-               img: "../assets/tomateImg.svg",
-               nome: "Outro Tomate",
-               prazoMinimo: "0",
-               prazoMaximo: "3",
-               descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
-            },
-         ]
-    },
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Outro Cozinheiro",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
+
+   {
+      nome: "Atendente",
+      produtos: [
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "1",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Tomate em Caixa",
+            prazoMinimo: "1",
+            prazoMaximo: "2",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+
+         {
+            img: "../assets/tomateImg.svg",
+            nome: "Outro Tomate",
+            prazoMinimo: "0",
+            prazoMaximo: "3",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus tellus arcu sit nibh consectetur."
+         },
+      ]
+   },
 ]
